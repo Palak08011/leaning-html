b926178f6383e0783b3fc830a7e2e1f4bf5b7c64
Palak08011 <palakkulhari11@gmail.com>
2024-04-26 11:18:14 +0530

    add

---
+++ Template.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".carousel_container");
+  const slides = document.querySelectorAll(".images");
+});
